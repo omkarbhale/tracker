04fc778211a273bc54b164d281b942651cdfb81c
John Doe <johndoe@example.com>
2025-04-21 22:10:49 +0530

    Add preload.js build step and copy non-js files for screenshot-desktop

--- scripts/build-main.js
+++ scripts/build-main.js
@@ -41,4 +41,32 @@ esbuild
 
 		return copyDir(srcAssetsPath, distAssetsPath);
 	})
+	.then(() => {
+		// Build preload.ts to preload.js
+		return esbuild.build({
+			entryPoints: ["src/main/preload.ts"],
+			bundle: true,
+			platform: "node",
+			outfile: "dist/preload.js",
+			external: ["electron", "node-api-dotnet"],
+			minify: false,
+		});
+	})
+	// For screenshot-desktop, copy the files from node_modules to dist
+	// They are not js, and not imported in the code, so esbuild does not copy them
+	// This is a workaround for the issue with esbuild not copying non-js files
+	.then(async () => {
+		const srcPath = path.join(
+			__dirname,
+			"../node_modules/screenshot-desktop/lib/win32"
+		);
+		const srcFiles = ["screenCapture_1.3.2.bat", "app.manifest"];
+		const destinationPath = path.join(__dirname, "../dist");
+		for (const file of srcFiles) {
+			const srcFilePath = path.join(srcPath, file);
+			const destFilePath = path.join(destinationPath, file);
+			console.log(`Copying ${srcFilePath} to ${destFilePath}`);
+			await fsp.copyFile(srcFilePath, destFilePath);
+		}
+	})
 	.catch(() => process.exit(1));
